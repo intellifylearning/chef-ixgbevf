729b063f48c854307c0847015cedf3d31d47dd39
Joseph Reynolds <reynoldsjoseph.f@gmail.com>
2016-10-13 14:25:21 -0400

    No yum

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures ixgbevf'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.1'
 
-depends          'yum-epel', '>= 0.0.0'
+#depends          'yum-epel', '>= 0.0.0'
 depends          'apt', '>= 0.0.0'
 
 %w(ubuntu centos redhat debian).each do |os|
