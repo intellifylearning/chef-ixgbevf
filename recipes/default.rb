65ca22a3e9f81a4ce44e9c97ee7bcefbf7f21978
jaredstehler <jared.stehler@intellifylearning.com>
2016-10-13 11:59:55 -0400

    applying patch from https://github.com/PaytmLabs/chef-ixgbevf/pull/3/files

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,8 +8,6 @@
 include_recipe 'ixgbevf::dkms'
 include_recipe 'ixgbevf::ifnames_disable' if node['ixgbevf']['disable_ifnames']
 
-package 'patch'
-
 remote_file "#{Chef::Config[:file_cache_path]}/#{node['ixgbevf']['package']}" do
   source node['ixgbevf']['package_url']
   checksum checksum
@@ -20,23 +18,7 @@ end
 execute 'extract_ixgbevf_source' do
   command "tar xzvf #{Chef::Config[:file_cache_path]}/#{node['ixgbevf']['package']}"
   cwd '/usr/src'
-  not_if { File.exists?( node['ixgbevf']['dir'] ) }
-end
-
-execute 'patch_ixgbevf_source' do
-  command "patch -p1 < /usr/src/ixgbevf-vlan-tx.patch"
-  cwd node['ixgbevf']['dir']
-  not_if { File.exists?( "#{node['ixgbevf']['dir']}/src/kcompat.h.orig" ) }
-  action :nothing
-end
-
-cookbook_file '/usr/src/ixgbevf-vlan-tx.patch' do
-  source 'ixgbevf-vlan-tx.patch'
-  owner 'root'
-  group node['root_group']
-  mode '0644'
-  action :create
-  notifies :run, 'execute[patch_ixgbevf_source]', :immediately
+  not_if { File.exist?( node['ixgbevf']['dir'] ) }
 end
 
 template "#{node['ixgbevf']['dir']}/dkms.conf" do
@@ -56,16 +38,22 @@ end
 execute 'dkms_add_ixgbevf' do
   command "dkms add -m ixgbevf -v #{node['ixgbevf']['version']}"
   # FIXME: does this vary by platform?
-  not_if { File.exists?( '/var/lib/dkms/ixgbevf' ) }
+  not_if { File.exist?(node['ixgbevf']['dkms_dir']) }
 end
 
 execute 'dkms_build_ixgbevf' do
   command "dkms build -m ixgbevf -v #{node['ixgbevf']['version']}"
+  not_if { File.exist?("#{node['ixgbevf']['dkms_dir']}/#{node['ixgbevf']['version']}/build") }
 end
 
-execute 'dkms_add_ixgbevf' do
+execute 'dkms_install_ixgbevf' do
   command "dkms install -m ixgbevf -v #{node['ixgbevf']['version']}"
+  not_if {`modinfo -F version ixgbevf`.strip == node['ixgbevf']['version']}
 end
 
-
+execute 'restart module' do
+  command "modprobe -r ixgbevf && modprobe ixgbevf"
+  action :nothing
+  subscribes :run, 'execute[dkms_install_ixgbevf]', :immediately
+end
 
